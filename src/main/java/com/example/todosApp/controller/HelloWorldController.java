18dfbb8ab2192b42851b597f64d7af9717a8b7c6
Noct2000 <oleg.kvasha.00@gmail.com>
2023-07-30 15:52:59 +0300

    [API]: add simple jsp

--- src/main/java/com/example/todosApp/controller/HelloWorldController.java
+++ src/main/java/com/example/todosApp/controller/HelloWorldController.java
@@ -1,14 +1,14 @@
 package com.example.todosApp.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
+@Controller()
 @RequestMapping("/hello")
 public class HelloWorldController {
     @GetMapping
     public String sayHello() {
-        return "Hello world\n";
+        return "hello";
     }
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>
+        hello
+    </title>
+</head>
+
+<body>
+<span style="color: blue; font-size: large">Hello</span>
+<span style="color: yellow; font-size: large">World</span>
+</body>
+
+</html>
